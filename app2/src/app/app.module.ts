8c23731c785a30d5a222af4a93ad7afd66976cba
Vasilis Plavos <vplavos@gmail.com>
2018-07-30 02:23:12 +0300

    L02.06 Server Component Activated in the App

--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
