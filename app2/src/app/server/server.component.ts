07d432e868eb7ef0ff103df4545654e743fd2ee6
Vasilis Plavos <vplavos@gmail.com>
2018-08-03 08:15:06 +0300

    ngClass

--- app2/src/app/server/server.component.ts
+++ app2/src/app/server/server.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+  .online {
+    color: white;
+  }
+  `]
 })
 export class ServerComponent {
   serverId: number = 10;
@@ -15,7 +20,7 @@ export class ServerComponent {
     return this.serverStatus;
   }
 
-  getColor(){
+  getColor() {
     return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
